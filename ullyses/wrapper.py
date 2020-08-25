49fe54f1186eb6a39b86a49ab0378311da2956c1
Jo Taylor <jotaylor@stsci.edu>
2020-08-25 11:40:14 -0400

    Change tabs to spaces, add argument parsing so user can specify input and output directory

--- ullyses/wrapper.py
+++ ullyses/wrapper.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import glob
 import numpy as np
@@ -12,47 +13,54 @@ This wrapper goes through each target folder in the ullyses data directory and f
 the COS data and which gratings are present. This info is then feeded into coadd.py.
 '''
 
-ULLYSESDIR = '/astro/ullyses/ULLYSES_DATA/'
-OUTPUTDIR = '/user/efrazer/ullyses/ullyses_dp/high_level_science_products/high_level_science_products/testcoadds/'
 
+def main(indir, outdir):
 
-def main():
+    for root, dirs, files in os.walk(indir, topdown=False):
 
-	for root, dirs, files in os.walk(ULLYSESDIR, topdown=False):
+        print(root)
+        targetname = root.split('/')[-1]
+        print(f"   {targetname}")
 
-		print(root)
-		targetname = root.split('/')[-1]
-		print(targetname)
+        # collect the gratings that we will loop through
+        # coadd.py will find the correct files itself,
+        # but we need to know which gratings are present
+        modes = []
 
-		# collect the gratings that we will loop through
-		# coadd.py will find the correct files itself,
-		# but we need to know which gratings are present
-		modes = []
+        for myfile in glob.glob(os.path.join(root, 'l*_x1d.fits')):  # only grabbing COS with the l*_x1d.fits
+            f1 = fits.open(myfile)
+            prihdr = f1[0].header
+            modes.append(prihdr['OPT_ELEM'])
 
-		for myfile in glob.glob(os.path.join(root, 'l*_x1d.fits')):  # only grabbing COS with the l*_x1d.fits
-			f1 = fits.open(myfile)
-			prihdr = f1[0].header
-			modes.append(prihdr['OPT_ELEM'])
+        if not modes:
+            print(f'No COS data to coadd for {targetname}.')
+            continue
 
-		if not modes:
-			print(f'No COS data to coadd for {targetname}.')
-			continue
+        uniqmodes = np.unique(modes)
 
-		uniqmodes = np.unique(modes)
+        for grating in uniqmodes:
 
-		for grating in uniqmodes:
+            # this initiates the class
+            prod = COSSegmentList(grating, path=root)
 
-			# this initiates the class
-			prod = COSSegmentList(grating, path=root)
+            # these two calls perform the main functions
+            prod.create_output_wavelength_grid()
+            prod.coadd()
 
-			# these two calls perform the main functions
-			prod.create_output_wavelength_grid()
-			prod.coadd()
-
-			# this writes the output file
-			prod.write(os.path.join(OUTPUTDIR, targetname + '_' + grating + '.fits'))
+            # this writes the output file
+            if not os.path.exists(outdir):
+                os.mkdir(outdir)
+            outname = os.path.join(outdir, targetname + '_' + grating + '.fits')
+            prod.write(outname)
+            print(f"   Wrote {outname}")
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--indir", default="/astro/ullyses/ULLYSES_DATA/",
+                        help="Directory(ies) with data to combine")
+    parser.add_argument("-o", "--outdir", default=".",
+                        help="Directory for output HLSPs")
+    args = parser.parse_args()
 
-	main()
+    main(args.indir, args.outdir)
