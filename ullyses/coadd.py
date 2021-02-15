047b22e50cf1cf62622080b6e5b877ee6171a547
Jo Taylor <jotaylor@stsci.edu>
2021-02-15 18:03:40 -0500

    Nest keymapping dictionaries, add method to combine_keys to return array of values, which is used for abut writing

--- ullyses/coadd.py
+++ ullyses/coadd.py
@@ -291,25 +291,25 @@ class SegmentList:
         hdr2 = fits.Header()
         hdr2['EXTNAME'] = ('PROVENANCE', 'Metadata for contributing observations')
         # set up the table columns
-        cfn = fits.Column(name='FILENAME', array=np.array([h["filename"] for h in self.primary_headers]), format='A32')
-        cpid = fits.Column(name='PROPOSID', array=np.array([h["proposid"] for h in self.primary_headers]), format='A32')
-        ctel = fits.Column(name='TELESCOPE', array=np.array([h["telescop"] for h in self.primary_headers]), format='A32')
-        cins = fits.Column(name='INSTRUMENT', array=np.array([h["instrume"] for h in self.primary_headers]), format='A32')
-        cdet = fits.Column(name='DETECTOR', array=np.array([h["detector"] for h in self.primary_headers]), format='A32')
-        cdis = fits.Column(name='DISPERSER', array=np.array([h["opt_elem"] for h in self.primary_headers]), format='A32')
-        ccen = fits.Column(name='CENWAVE', array=np.array([h["cenwave"] for h in self.primary_headers]), format='A32')
-        cap = fits.Column(name='APERTURE', array=np.array([h["aperture"] for h in self.primary_headers]), format='A32')
-        csr = fits.Column(name='SPECRES', array=np.array([h["specres"] for h in self.primary_headers]), format='F8.1')
-        ccv = fits.Column(name='CAL_VER', array=np.array([h["cal_ver"] for h in self.primary_headers]), format='A32')
-        mjd_begs = np.array([h["expstart"] for h in self.first_headers])
-        mjd_ends = np.array([h["expend"] for h in self.first_headers])
+        cfn = fits.Column(name='FILENAME', array=self.combine_keys("filename", "arr"), format='A32')
+        cpid = fits.Column(name='PROPOSID', array=self.combine_keys("proposid", "arr"), format='A32')
+        ctel = fits.Column(name='TELESCOPE', array=self.combine_keys("telescop", "arr"), format='A32')
+        cins = fits.Column(name='INSTRUMENT', array=self.combine_keys("instrume", "arr"), format='A32')
+        cdet = fits.Column(name='DETECTOR', array=self.combine_keys("detector", "arr"), format='A32')
+        cdis = fits.Column(name='DISPERSER', array=self.combine_keys("opt_elem", "arr"), format='A32')
+        ccen = fits.Column(name='CENWAVE', array=self.combine_keys("cenwave", "arr"), format='A32')
+        cap = fits.Column(name='APERTURE', array=self.combine_keys("aperture", "arr"), format='A32')
+        csr = fits.Column(name='SPECRES', array=self.combine_keys("specres", "arr"), format='F8.1')
+        ccv = fits.Column(name='CAL_VER', array=self.combine_keys("cal_ver", "arr"), format='A32')
+        mjd_begs = self.combine_keys("expstart", "arr")
+        mjd_ends = self.combine_keys("expend", "arr")
         mjd_mids = (mjd_ends + mjd_begs) / 2.
         cdb = fits.Column(name='MJD_BEG', array=mjd_begs, format='F15.9', unit='d')
         cdm = fits.Column(name='MJD_MID', array=mjd_mids, format='F15.9', unit='d')
         cde = fits.Column(name='MJD_END', array=mjd_ends, format='F15.9', unit='d')
-        cexp = fits.Column(name='XPOSURE', array=np.array([h["exptime"] for h in self.first_headers]), format='F15.9', unit='s')
-        cmin = fits.Column(name='MINWAVE', array=np.array([h["minwave"] for h in self.primary_headers]), format='F9.4', unit='Angstrom')
-        cmax = fits.Column(name='MAXWAVE', array=np.array([h["maxwave"] for h in self.primary_headers]), format='F9.4', unit='Angstrom')
+        cexp = fits.Column(name='XPOSURE', array=self.combine_keys("exptime", "arr"), format='F15.9', unit='s')
+        cmin = fits.Column(name='MINWAVE', array=self.combine_keys("minwave", "arr"), format='F9.4', unit='Angstrom')
+        cmax = fits.Column(name='MAXWAVE', array=self.combine_keys("maxwave", "arr"), format='F9.4', unit='Angstrom')
     
         cd2 = fits.ColDefs([cfn, cpid, ctel, cins, cdet, cdis, ccen, cap, csr, ccv, cdb, cdm, cde, cexp, cmin ,cmax])
     
@@ -386,50 +386,53 @@ class SegmentList:
                                       
                                       
     def combine_keys(self, key, method):
-        keymap_hst = {"expstart": ("expstart", 1),
-                      "expend": ("expend", 1),
-                      "exptime": ("exptime", 1),
-                      "telescop": ("telescop", 0),
-                      "instrume": ("instrume", 0),
-                      "detector": ("detector", 0),
-                      "opt_elem": ("opt_elem", 0),
-                      "cenwave": ("cenwave", 0),
-                      "aperture": ("aperture", 0),
-                      "obsmode": ("obsmode", 0),
-                      "proposid": ("proposid", 0),
-                      "centrwv": ("centrwv", 0),
-                      "minwave": ("minwave", 0),
-                      "maxwave": ("maxwave", 0)}
-        keymap_fuse = {"expstart": ("obsstart", 0),
-                       "expend": ("obsend", 0),
-                       "exptime": ("obstime", 0),
-                       "telescop": ("telescop", 0),
-                       "instrume": ("instrume", 0),
-                       "detector": ("detector", 0),
-                       "opt_elem": ("detector", 0),
-                       "cenwave": ("centrwv", 0),
-                       "aperture": ("aperture", 0),
-                       "obsmode": ("instmode", 0),
-                       "proposid": ("prgrm_id", 0),
-                       "centrwv": ("centrwv", 0),
-                       "minwave": ("wavemin", 0),
-                       "maxwave": ("wavemax", 0)}
-
-        if self.instrument == "FUSE":
-            keymap = keymap_fuse
-        else:
-            keymap = keymap_hst
-        actual_key = keymap[key][0]
-        hdrno = keymap[key][1]
-
-        if hdrno == 0:
-            hdrs = self.primary_headers
-        else:
-            hdrs = self.first_headers
-
-        vals = [h[actual_key] for h in hdrs]
+        keymap= {"HST": {"expstart": ("expstart", 1),
+                         "expend": ("expend", 1),
+                         "exptime": ("exptime", 1),
+                         "telescop": ("telescop", 0),
+                         "instrume": ("instrume", 0),
+                         "detector": ("detector", 0),
+                         "opt_elem": ("opt_elem", 0),
+                         "cenwave": ("cenwave", 0),
+                         "aperture": ("aperture", 0),
+                         "obsmode": ("obsmode", 0),
+                         "proposid": ("proposid", 0),
+                         "centrwv": ("centrwv", 0),
+                         "minwave": ("minwave", 0),
+                         "maxwave": ("maxwave", 0),
+                         "filename": ("filename", 0),
+                         "specres": ("specres", 0),
+                         "cal_ver": ("cal_ver", 0)},
+                "FUSE": {"expstart": ("obsstart", 0),
+                         "expend": ("obsend", 0),
+                         "exptime": ("obstime", 0),
+                         "telescop": ("telescop", 0),
+                         "instrume": ("instrume", 0),
+                         "detector": ("detector", 0),
+                         "opt_elem": ("detector", 0),
+                         "cenwave": ("centrwv", 0),
+                         "aperture": ("aperture", 0),
+                         "obsmode": ("instmode", 0),
+                         "proposid": ("prgrm_id", 0),
+                         "centrwv": ("centrwv", 0),
+                         "minwave": ("wavemin", 0),
+                         "maxwave": ("wavemax", 0),
+                         "filename": ("filename", 0),
+                         "specres": ("spec_rp", 1),
+                         "cal_ver": ("cf_vers", 0)}}
         
-        # Allowable methods are min, max, average, sum, multi
+        vals = []
+        for i in range(len(self.primary_headers)):
+            tel = self.primary_headers[i]["telescop"]
+            actual_key = keymap[tel][key][0]
+            hdrno = keymap[tel][key][1]
+            if hdrno == 0:
+                val = self.primary_headers[i][actual_key]
+            else:
+                val = self.first_headers[i][actual_key]
+            vals.append(val)
+
+        # Allowable methods are min, max, average, sum, multi, arr
         if method == "multi":
             keys_set = list(set(vals))
             if len(keys_set) > 1:
@@ -444,6 +447,8 @@ class SegmentList:
             return np.average(vals)
         elif method == "sum":
             return np.sum(vals)
+        elif method == "arr":
+            return np.array(vals)
 
 class STISSegmentList(SegmentList):
 
