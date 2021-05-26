7ad098e250948278d8e36fcc825c07dbc0f1eb47
Elaine Frazer <efrazer@stsci.edu>
2021-05-26 10:26:36 -0400

    more thoughts

--- ullyses/splittag_wrapper.py
+++ ullyses/splittag_wrapper.py
@@ -1,9 +1,37 @@
 import os
 import calcos
 from costools import splittag
+import glob
 
 """
-This code is a wrapper for the 
-
+This code will:
+(1) use costools.splittag to divide corrtag files for the variable stars into multiple corrtags based on a time interval
+(2) run calcos on the output corrtags to re-extract them into x1ds (x1dsums?)
+(3) remove unnecessary file outputs such as counts images to maintain a tidy directory
 
 """
+
+DATADIR = '/astro/ullyses/ULLYSES_DATA/'
+OUTPUTDIR = '/astro/ullyses/'
+
+
+
+
+splittag.splittag("rootname_corrtag_a.fits", "split250s",
+                  starttime=None, increment=None, endtime=None,
+                  time_list="0, 250, 500, 750, 1000")
+
+
+
+if __name__ == "__main__":
+
+    # need to get the names of all the targets we will be making lightcurves for
+    # probably a better way to do this than hard-coding. will revisit
+    targetnamelist = ['TW-HYA']
+
+    for target in targetnamelist:
+
+        targetdir = os.path.join(DATADIR, target)
+
+        corrtags = glob.glob(os.path.join(targetdir, '*corrtag_a*.fits'))
+
