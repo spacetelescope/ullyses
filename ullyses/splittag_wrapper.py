e99ff7ac74cd8f58a737162c1a9c8b505df6096b
Elaine Snyder <esnyder@stsci.edu>
2021-07-21 14:44:11 -0400

    added comments and fixed paths to move outputs

--- ullyses/splittag_wrapper.py
+++ ullyses/splittag_wrapper.py
@@ -18,10 +18,9 @@ This code will:
 
 
 def parseargs():
-
     """
     Parse command line options
-    :return:
+    :return: None
     """
 
     parser = argparse.ArgumentParser()
@@ -29,9 +28,9 @@ def parseargs():
     parser.add_argument('-o', '--outdir', required=True, help='Directory for the splittag and calcos output.')
 
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-t', '--timelist', required=True,
+    group.add_argument('-t', '--timelist',
                        help='String list of times to split exposures over. Ex: "0, 20, 40, 60".')
-    group.add_argument('-s', '--startstopincr', required=True,
+    group.add_argument('-s', '--startstopincr',
                        help='String list of start time, stop time, and increment to split '
                             'exposures over. Ex: "0, 250, 50".')
 
@@ -51,56 +50,62 @@ def parseargs():
     return args.indir, args.outdir, args.timelist, starttime, stoptime, increment, args.clobber, args.ncores
 
 
-def clobberfiles(outputdir):
-
+def clobberfiles(outputfolder):
     """
     Remove old output files
-    :param outputdir: output directory to remove files from
+    :param outputfolder: Output directory to remove files from
+    :return: None
     """
 
-    rmfiles = glob.glob(os.path.join(outputdir, '*'))
+    rmfiles = glob.glob(os.path.join(outputfolder, 'calcosout', '*.fits')) +\
+              glob.glob(os.path.join(outputfolder, '*.fits'))
     if len(rmfiles):
-        print('Clobbering old output from {}'.format(outputdir))
+        print('Clobbering old output from {}'.format(outputfolder))
         for file in rmfiles:
             os.remove(file)
 
 
-def setupdirs(rootdir, outputdir, clobber):
-
+def setupdirs(rootdir, outputfolder, clobber):
     """
     Checks if output directory exists and if not, creates it
     Clobbers old files in output directory if specified
-    :param outputdir: user specified output dir
-    :param clobber: Boolean switch to clean dir or not
+    :rootdir: Current working directory
+    :param outputfolder: User specified output directory
+    :param clobber: Boolean switch to clean directory or not
+    :return: None
     """
 
-    if not os.path.isdir(outputdir):
-        os.mkdir(os.path.join(rootdir, outputdir))
+    if not os.path.isdir(outputfolder):
+        os.mkdir(os.path.join(rootdir, outputfolder))
 
     if clobber:
-        clobberfiles(os.path.join(rootdir, outputdir))
+        clobberfiles(os.path.join(rootdir, outputfolder))
 
 
-def mvsplittagoutput(cwdir, outdir):
+def mvsplittagoutput(rootdir, outputfolder):
+    """
+    The splittag output is by default placed into the current working directory
+    This moves the output into the specified output directory
+    :param rootdir: Current working directory
+    :param outputfolder: Output directory
+    :return: None
+    """
 
-    outfiles = glob.glob(os.path.join(cwdir, '*split*'))
+    outfiles = glob.glob(os.path.join(rootdir, '*split*.fits'))
 
     for myfile in outfiles:
-
-        os.replace(myfile, os.path.join())
-
+        os.replace(myfile, myfile.replace(rootdir, os.path.join(rootdir, outputfolder)))
 
 
 def runsplittag(inputfile, starttime, increment, endtime, timelist):
-
     """
-    Runs the splittag command. Needs inputs of either start, stop, and increment or timelist to run.
-    :param inputfile: corrtag filename
-    :param starttime: the time to start the split
-    :param increment: time increment of split
-    :param endtime: the time to end the split
-    :param timelist: string of times to split over. ex: "0, 10, 20, 30, 40"
-
+    Runs the splittag command
+    Needs inputs of either start, stop, and increment or timelist to run
+    :param inputfile: Corrtag filename
+    :param starttime: Time to start the split
+    :param increment: Time increment of split
+    :param endtime: Tme to end the split
+    :param timelist: String of times to split over. ex: "0, 10, 20, 30, 40"
     :return: None
     """
 
@@ -111,34 +116,39 @@ def runsplittag(inputfile, starttime, increment, endtime, timelist):
                       starttime=starttime, increment=increment, endtime=endtime,
                       time_list=timelist)
 
-    mvsplittagoutput()
 
-def calibrate_files(outputfolder, item):
+def calibratefiles(outputfolder, item):
     """
     Runs calcos on specified files
-    :param outputfolder:
-    :param item:
-    :return:
+    :param outputfolder: folder to place calcos output in
+    :param item: corrtag file to calibrate
+    :return: None
     """
 
     calcos.calcos(item, outdir=outputfolder)
 
 
-def parallel_cal(files, outputfolder, ncores):
+def parallelcal(files, outputfolder, ncores):
     """
     Parallelizes calcos
-    :param files:
-    :param outputfolder:
-    :param ncores:
-    :return:
+    :param files: Corrtag files to calibrate
+    :param outputfolder: Folder to place calcos output in
+    :param ncores: Number of cores for parallelization
+    :return: None
     """
 
     pool = mp.Pool(processes=ncores)
-    calfunc = partial(calibrate_files, outputfolder)
+    calfunc = partial(calibratefiles, outputfolder)
     pool.map(calfunc, files)
 
 
 def rmlargefiles(outputdir):
+    """
+    Removes the FLT and COUNT files from the output directory
+    These files are large in size and are rarely used
+    :param outputdir: CalCOS output directory
+    :return: None
+    """
 
     fltfiles = glob.glob(os.path.join(outputdir, '*flt*'))
     for file in fltfiles:
@@ -150,25 +160,43 @@ def rmlargefiles(outputdir):
 
 if __name__ == "__main__":
 
+    # collect the command line arguments
     indir, outdir, tlist, start, end, incr, clob, ncores = parseargs()
 
+    # get current working directory
     cwdir = os.getcwd()
 
+    # create the output directory if it doesn't exist,
+    # and remove old output if clob=True
     setupdirs(cwdir, outdir, clob)
 
+    # collect all the corrtags to split
     corrtagfiles = glob.glob(os.path.join(cwdir, indir, '*corrtag*'))
 
-    for corrtag in corrtagfiles:
+    # g160m = [x for x in corrtagfiles if fits.getval(x, 'opt_elem') == 'G160M']
+    g230l = [x for x in corrtagfiles if fits.getval(x, 'opt_elem') == 'G230L']
+
+    # run splittag on all the corrtags
+    for corrtag in g230l:
         runsplittag(corrtag, start, incr, end, tlist)
 
+    # splittag outputs to the cwd, so move to the right output folder
+    mvsplittagoutput(cwdir, outdir)
+
+    # collect all the split corrtag files
     splitcorrtagfiles = glob.glob(os.path.join(cwdir, outdir, '*split*'))
 
+    # filter them to not include the b segment
+    splitcorrtagfiles_nob = [x for x in splitcorrtagfiles if '_b' not in x]
+
+    # run calcos on the split corrtags in parallel
     if ncores is not None:
+        parallelcal(splitcorrtagfiles_nob, os.path.join(cwdir, outdir, 'calcosout'), ncores=ncores)
 
-        parallel_cal(splitcorrtagfiles, os.path.join(cwdir, outdir), ncores=ncores)
-
+    # run calcos on the split corrtags without parallelizing
     else:
+        for splitfile in splitcorrtagfiles_nob:
+            calibratefiles(os.path.join(cwdir, outdir, 'calcosout'), splitfile)
 
-        calibrate_files(os.path.join(cwdir, outdir), splitcorrtagfiles)
-
+    # remove the FLT and COUNTS files
     rmlargefiles(os.path.join(cwdir, outdir))
