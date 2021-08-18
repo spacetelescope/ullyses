095e873a9ea95d9c9925967ec0a0ff31ac0e0bfa
Jo Taylor <jotaylor@stsci.edu>
2021-08-18 18:43:41 -0400

    Initial commit

---
+++ ullyses/apply_cos_shifts.py
@@ -0,0 +1,45 @@
+from astropy.io import fits
+import calcos
+import os
+import shutil
+import glob
+import pandas as pd
+
+SHIFTS = {"sz10": "cos_shift_files/sz10_shifts.txt"}
+CUSTOM_DIR = "/astro/ullyses/custom_cal"
+VETTED_DIR = "/astro/ullyses/all_vetted_data_dr3"
+
+def apply_shifts():
+    for targ,shift_file in SHIFTS.items():
+        seen = []
+        df = pd.read_csv(shift_file, delim_whitespace=True, 
+            names=["rootname", "fppos", "flash", "segment", "shift1"], 
+            header=None)
+        roots = df["rootname"]
+        for root in roots:
+            ipppss = root[:6]
+            if ipppss not in seen:
+                seen.append(ipppss)
+            else:
+                continue
+            files = glob.glob(os.path.join(CUSTOM_DIR, targ,  
+                          f"{ipppss}*asn.fits"))
+            assert len(files) != 0, f"No files found for {targ}"
+            outdir = os.path.join(CUSTOM_DIR, targ, "cos_shifted")
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)
+            calcos.calcos(files[0], shift_file=shift_file, outdir=outdir)
+            x1ds = glob.glob(os.path.join(outdir, "*x1d.fits"))
+            copydir = os.path.join(VETTED_DIR, targ)
+            for x1d in x1ds:
+                existing = os.path.join(copydir, os.path.basename(x1d))
+                if os.path.exists(existing):
+                    os.remove(existing)
+                with fits.open(x1d, mode="update") as hdulist:
+                    hdulist[0].header["HLSP_LVL"] = 0
+                shutil.copy(x1d, copydir)
+            print(f"Copied custom x1ds to {copydir}")
+    print(f"Copied custom x1ds to {VETTED_DIR}")
+
+if __name__ == "__main__":
+    apply_shifts()
