cebfbcd037123ceabec53126767b8fc713267e33
Robert Jedrzejewski <rij@stsci.edu>
2023-07-27 11:36:50 -0400

    WIP: fixing regression tests to pull from MAST

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -12,10 +12,34 @@ from ullyses import wrapper
 from ullyses_utils.ullyses_config import VERSION
 import ullyses_utils
 
-ULLYSES_DATA_LOCATION = 'https://ullyses.stsci.edu/files/'
+'''
+curl -L -X GET "https://mast.stsci.edu/api/v0.1/Download/file?uri=mast%3AHLSP%2Fullyses%2Fsk-70d16%2Fdr6%2Fhlsp_ullyses_hst_cos_sk-70d16_g130m-g160m-g185m_dr6_cspec.fits" --output "MAST_2023-06-29T15_31_07.292Z/ULLYSES/SK-70D16/hlsp_ullyses_hst_cos_sk-70d16_g130m-g160m-g185m_dr6_cspec.fits" --create-dirs
 
-COS_TARGETS = ['sk-70d16',
-               'n11-els-018']
+curl -L -X GET "https://mast.stsci.edu/api/v0.1/Download/file?uri=mast%3AHLSP%2Fullyses%2Fsk-70d16%2Fdr6%2Fhlsp_ullyses_hst_cos_sk-70d16_g130m_dr6_cspec.fits" --output "MAST_2023-06-29T15_31_07.292Z/ULLYSES/SK-70D16/hlsp_ullyses_hst_cos_sk-70d16_g130m_dr6_cspec.fits" --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/api/v0.1/Download/file?uri=mast%3AHLSP%2Fullyses%2Fsk-70d16%2Fdr6%2Fhlsp_ullyses_hst_cos_sk-70d16_g160m_dr6_cspec.fits" --output "MAST_2023-06-29T15_31_07.292Z/ULLYSES/SK-70D16/hlsp_ullyses_hst_cos_sk-70d16_g160m_dr6_cspec.fits" --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/api/v0.1/Download/file?uri=mast%3AHLSP%2Fullyses%2Fsk-70d16%2Fdr6%2Fhlsp_ullyses_hst_cos_sk-70d16_g185m_dr6_cspec.fits" --output "MAST_2023-06-29T15_31_07.292Z/ULLYSES/SK-70D16/hlsp_ullyses_hst_cos_sk-70d16_g185m_dr6_cspec.fits" --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=LE9R4C010%2Fle9r4c010_asn.fits" --output "MAST_2023-06-15T15_37_17.804Z/HST/LE9R4C010/le9r4c010_asn.fits" --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=LE9R4C010%2Fle9r4cx1q_x1d.fits"
+
+'''
+
+COS_TARGETS = {'sk-70d16': {'truth': ['hlsp_ullyses_hst_cos_sk-70d16_g130m-g160m-g185m_dr6_cspec.fits',
+                                      'hlsp_ullyses_hst_cos_sk-70d16_g130m_dr6_cspec.fits',
+                                      'hlsp_ullyses_hst_cos_sk-70d16_g160m_dr6_cspec.fits',
+                                      'hlsp_ullyses_hst_cos_sk-70d16_g185m_dr6_cspec.fits'],
+                            'input': ['le9r4cwyq_x1d.fits', 'le9r4cx1q_x1d.fits',
+                                      'le9r4cxeq_x1d.fits', 'le9r4cx7q_x1d.fits',
+                                      'le9r4cx4q_x1d.fits', 'le9r4cxaq_x1d.fits',
+                                      'le9r4cxpq_x1d.fits', 'le9r4cxtq_x1d.fits',
+                                      'le9r4cxxq_x1d.fits', 'le9r4cxzq_x1d.fits',
+                                      'le9r4cxiq_x1d.fits', 'le9r4cxmq_x1d.fits',
+                                      'le9r4cy2q_x1d.fits', 'le9r4cxkq_x1d.fits']},
+               'n11-els-018': {'truth': [],
+                               'input': []}}
 
 STIS_TARGETS = ['lh9-34',
                 'pgmw3120']
@@ -23,11 +47,13 @@ STIS_TARGETS = ['lh9-34',
 MULTIPLE_TARGETS = ['sk-67d22',
                     'sk-68d26']
 
+RELEASE = 'dr6'
+
 class TestWrapper():
 
     def test_cos_data(self):
         for target in COS_TARGETS:
-            self.setup_tree(target)
+            self.setup_tree(COS_TARGETS[target])
             self.run_wrapper('./')
             report = self.compare_outputs()
             self.cleanup(target)
@@ -58,23 +84,31 @@ class TestWrapper():
                 raise AssertionError(report)
         return
 
-    def setup_tree(self, target):
+    def setup_tree(self, target, targetdict, release):
         if os.path.isdir(target):
             shutil.rmtree(target)
         if os.path.isdir('truth'):
             shutil.rmtree('truth')
-        filename = target + '.tar.gz'
-        fullurl = ULLYSES_DATA_LOCATION + filename
-        print("Retrieving {}".format(fullurl))
-        urllib.request.urlretrieve(fullurl, filename)
-        tarball = tarfile.open(filename)
-        tarball.extractall()
-        os.mkdir('truth')
-        os.chdir(target)
-        hlsps = glob.glob('hlsp_ullyses*')
-        for datafile in hlsps:
-            os.rename(datafile,'../truth/'+datafile)
+        for ufile in targetdict['truth']:
+            fullurl = self.construct_ullyses_url(target, ufile, release)
+            print("Retrieving {}".format(fullurl))
+            urllib.request.urlretrieve(fullurl, ufile)
+#        os.mkdir('truth')
+#        os.chdir(target)
+#        hlsps = glob.glob('hlsp_ullyses*')
+#        for datafile in hlsps:
+#            os.rename(datafile,'../truth/'+datafile)
         return
+
+    def construct_ullyses_url(self, target, filename, release):
+        firstpart = 'https://mast.stsci.edu/api/v0.1/Download/file?uri=mast%3AHLSP%2Fullyses%2F'
+        fullurl = firstpart + target + '%2F' + release + '%2F' + filename
+        return fullurl
+
+    def construct_rawdata_url(self, filename):
+        asn_id = fits.getval(filename, 'ASN_ID')
+        fullurl = "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=" + asn_id + "%2F" + filename
+        return fullurl
 
     def run_wrapper(self, indir):
         wrapper.main(indir, outdir=indir, version=VERSION)
