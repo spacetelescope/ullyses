2bb55e0b0ffedd3a6feeb2eecd2b294e7749f958
Jo Taylor <jotaylor@stsci.edu>
2023-10-21 13:52:52 -0400

    Add level0 keyword for coadd script

--- ullyses/stisblazefix_wrapper.py
+++ ullyses/stisblazefix_wrapper.py
@@ -4,14 +4,18 @@ Apply the stisblazefix to selected echelle data.
 
 from stisblazefix import fluxfix
 import pandas as pd
+from astropy.io import fits
 
 import ullyses_utils
 
 
 def apply_sbf(filename, pdfname = '/dev/null', verbose=True):
     fluxfix([filename], pdfname=pdfname)
+    newfile = filename.replace("x1d.fits", "x1f.fits")
+    with fits.open(newfile, mode="update") as hdulist:
+    # Since custom processing was performed, mark these as level0
+        hdulist[0].header["HLSP_LVL"] = 0
     if verbose is True:
-        newfile = filename.replace("x1d.fits", "x1f.fits")
         print(f"Wrote {newfile}")
 
 
