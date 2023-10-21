f1737043cc43e6376444e80b58af835616bea4f5
Jo Taylor <jotaylor@stsci.edu>
2023-10-21 13:06:18 -0400

    Add more print statements

--- ullyses/stisblazefix_wrapper.py
+++ ullyses/stisblazefix_wrapper.py
@@ -8,11 +8,15 @@ import pandas as pd
 import ullyses_utils
 
 
-def apply_sbf(filename, pdfname = '/dev/null'):
+def apply_sbf(filename, pdfname = '/dev/null', verbose=True):
     fluxfix([filename], pdfname=pdfname)
+    if verbose is True:
+        newfile = filename.replace("x1d.fits", "x1f.fits")
+        print(f"Wrote {newfile}")
 
 
 def ullyses_sbf():
+    print("Running stisblazefix...")
     ull_dbfile = ullyses_utils.__path__[0] + '/data/target_metadata/ullyses_db.csv'
     db = pd.read_csv(ull_db)
     files = db.loc[db["sbf"] == True]
