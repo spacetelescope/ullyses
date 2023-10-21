f047424dfe6c98ab34ac31a3dc3e8d340a524e02
Jo Taylor <jotaylor@stsci.edu>
2023-10-21 16:31:41 -0400

    Create generic wrapper for the coadd method, this can be used with any COS/STIS/FUSE data

---
+++ ullyses/generic_coadd_wrapper.py
@@ -0,0 +1,64 @@
+from astropy.io import fits
+from collections import defaultdict
+import os
+
+from ullyses.coadd import COSSegmentList, STISSegmentList, FUSESegmentList, CCDSegmentList  
+
+def coadd_files(infiles, outdir, outfile=None, clobber=False):
+     
+    nonvofiles = [x for x in infiles if "_vo.fits" not in x]
+    vofiles = [x for x in infiles if "_vo.fits" in x]
+    # collect the gratings that we will loop through
+    # coadd.py will find the correct files itself,
+    # but we need to know which gratings are present
+    uniqmodes = defaultdict(list)
+    
+    for infile in nonvofiles:
+        prihdr = fits.getheader(infile)
+        obsmode = (prihdr['INSTRUME'], prihdr['OPT_ELEM'], prihdr['DETECTOR'])
+        uniqmodes[obsmode].append(infile)
+
+    if vofiles:
+        if len(vofiles) != 1:
+            print("More than 1 FUSE data file, aborting")
+        else:
+            obsmode = ('FUSE', 'FUSE', 'FUSE')
+            uniqmodes[obsmode].append(vofiles[0])
+
+    if not uniqmodes:
+        print(f'No data to coadd, EXITING')
+        return
+
+    level = 2
+    for obskey in uniqmodes:
+        instrument, grating, detector = obskey
+        infiles = uniqmodes[obskey]
+        # this instantiates the class
+        if instrument == 'COS':
+            prod = COSSegmentList(grating, infiles=infiles)
+        elif instrument == 'STIS':
+            if detector == 'CCD':
+                prod = CCDSegmentList(grating, infiles=infiles)
+            else:
+                prod = STISSegmentList(grating, infiles=infiles)
+        elif instrument == 'FUSE':
+            prod = FUSESegmentList(grating, infiles=infiles)
+        else:
+            print(f'Unknown mode [{instrument}, {grating}, {detector}]')
+            return
+        if len(prod.members) > 0:
+            if outdir is None:
+                outdir = indir
+            if outfile is None:
+                outfile = f"{instrument.lower()}_{grating.lower()}_coadd.fits"
+                outfile = os.path.join(outdir, outfile)
+            prod.create_output_wavelength_grid()
+            prod.coadd()
+            prod.target = prod.get_targname()
+            prod.targ_ra, prod.targ_dec = prod.get_coords()
+            prod.write(outfile, clobber)
+            print(f"Wrote {outfile}")
+
+files = ["/astro/ullyses/all_vetted_data_dr6/av-456/lbp507rtq_x1d.fits", "/astro/ullyses/all_vetted_data_dr6/av-456/lbp507rtq_x1d.fits", "/astro/ullyses/all_vetted_data_dr6/av-456/lbp507rtq_x1d.fits"]
+
+coadd_files(files, "test_out_coaddfiles", clobber=True)
