ae00f081552e42c5a31cd0c9bd798b864b062004
Jo Taylor <jotaylor@stsci.edu>
2023-10-22 14:45:45 -0400

    Remove wrapper, which will be in DP repo. Allow moving output file to new directory

--- ullyses/stisblazefix_wrapper.py
+++ ullyses/stisblazefix_wrapper.py
@@ -5,25 +5,21 @@ Apply the stisblazefix to selected echelle data.
 from stisblazefix import fluxfix
 import pandas as pd
 from astropy.io import fits
-
+import shutil
 import ullyses_utils
 
 
-def apply_sbf(filename, pdfname = '/dev/null', verbose=True):
-    fluxfix([filename], pdfname=pdfname)
-    newfile = filename.replace("x1d.fits", "x1f.fits")
-    with fits.open(newfile, mode="update") as hdulist:
+def apply_sbf(infile, outfile=None, pdfname='/dev/null', verbose=True):
+    fluxfix([infile], pdfname=pdfname)
+    newfile = infile.replace("x1d.fits", "x1f.fits")
+    if outfile is not None:
+        shutil.move(newfile, outfile)
+    else:
+        outfile = newfile
+    with fits.open(outfile, mode="update") as hdulist:
     # Since custom processing was performed, mark these as level0
         hdulist[0].header["HLSP_LVL"] = 0
     if verbose is True:
-        print(f"Wrote {newfile}")
+        print(f"Wrote {outfile}")
 
-
-def ullyses_sbf():
-    print("Running stisblazefix...")
-    ull_dbfile = ullyses_utils.__path__[0] + '/data/target_metadata/ullyses_db.csv'
-    db = pd.read_csv(ull_db)
-    files = db.loc[db["sbf"] == True]
-    for item in files:
-        apply_sbf(item)
 
